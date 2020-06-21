dc9b69ccb1daba9aebb3e79b38db9cb900cf3d05
ShimmyMySherbet <ShimmyMySherbet@gmail.com>
2020-06-22 01:09:32 +1000

    Remove debug scripts

--- PatchMod/Scripts/Patch.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-taskkill /f /t /im Unturned.exe
-copy "C:\Users\User\source\repos\PatchMod\PatchMod\bin\Debug\PatchMod.dll" "D:\Servers\DevServers\Unturned\unturned\Modules\PatchMod\PatchMod.dll"
-if NOT [%1]==[noexit] exit
--- PatchMod/Scripts/StartServer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call Patch.bat noexit
-pushd D:\Servers\DevServers\Unturned\unturned\
-Unturned.exe -nographics -batchmode +secureserver/CleanServer
-echo Unturned Client closed.
-Timeout /T 4
